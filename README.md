eb48d39b1c9efc1e254bdc47c288cc1dcfd42a38
gabricc <g.carvalho@slangapp.com>
2024-02-28 06:54:09 -0500

    update steps

---
+++ README.md
@@ -0,0 +1,9 @@
+To get started with this project make sure you have [vagrant](https://developer.hashicorp.com/vagrant/install?product_intent=vagrant) and [virtualbox](https://www.virtualbox.org/wiki/Downloads) installed in your machines, then run: `vagrant up`. 
+
+Then go through the .md files following the numerical order.
+
+In the end you should be able to run a container without using a container runtime.
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+This project was inspired in this awesome [documentation](https://vitalino.gitbook.io/criando-um-container-linux-na-unha) from Vitalino.
--- provision.sh
+++ provision.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+## Install required packages
+echo -e "\n" | add-apt-repository universe
+apt update; apt install cgroup-tools debootstrap stress lynx cg-tools -y
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+## Download debian image
+debootstrap stable ./debian http://deb.debian.org/debian/
 
-# Install docker packages
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+## Switch from CGroup v2 to v1 (required to overcome this error: "cgroups can't be listed: Cgroup is not mounted")
+cp /etc/default/grub /etc/default/grub_bkp
+sed -i '/^GRUB_CMDLINE_LINUX/ s/"$/ systemd.unified_cgroup_hierarchy=0"/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install lynx
-sudo apt install lynx -y
+##### Use this in case you want to extract an image using docker #####
 
-cat << EOF >> Dockerfile
-FROM centos:latest
-RUN cd /etc/yum.repos.d/
-RUN sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* \
-    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-RUN dnf install python3 -y
-EOF
-
-docker build -t centos-local:latest .
-
-## Extracting image from docker
-docker run --name imagem centos:latest
-docker export imagem > centos.tar
-
-sudo mkdir /mnt/{image,upper,work,merged}
-sudo tar -xvf centos.tar -C /mnt/image/
-
-## Stop docker
-sudo systemctl stop docker
+## Add Docker's official GPG key:
+#sudo apt-get update
+#sudo apt-get install ca-certificates curl
+#sudo install -m 0755 -d /etc/apt/keyrings
+#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#sudo chmod a+r /etc/apt/keyrings/docker.asc
+#
+## Add the repository to Apt sources:
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+#
+## Install docker packages
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+#
+## Install lynx
+#sudo apt install lynx -y
+#
+#cat << EOF >> Dockerfile
+#FROM centos:latest
+#RUN cd /etc/yum.repos.d/
+#RUN sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* \
+#    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+#RUN dnf install python3 -y
+#EOF
+#
+#docker build -t centos-local:latest .
+#
+### Extracting image from docker
+#docker run --name imagem centos:latest
+#docker export imagem > centos.tar
+#
+#sudo mkdir /mnt/{image,upper,work,merged}
+#sudo tar -xvf centos.tar -C /mnt/image/
+#
+### Stop docker
+#sudo systemctl stop docker
